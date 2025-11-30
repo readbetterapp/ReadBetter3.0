215573bb375f5e085605ce7f303567adfa4df83b
Ermin Tupkovic <readbetterapp@gmail.com>
2025-11-30 12:15:22 +1100

    Initial Commit

---
+++ ReadBetterApp3.0/ReadBetterApp3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ReadBetterApp3_0App.swift
+//  ReadBetterApp3.0
+//
+//  Created by Ermin on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReadBetterApp3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
